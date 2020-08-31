279608ffaeb381cda5e083793ebfb25595f40eb7
lifei <1058235935@qq.com>
2020-08-31 18:53:40 +0800

    修改boot脚本

--- boot.sh
+++ boot.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # this script is used to boot a Docker container
-cd /opt/flask-quickstart
+cd /opt/flask-quickstart || return
 cp .env.template .env
 while true; do
-    flask db upgrade
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo Deploy command failed, retrying in 5 secs...
-    sleep 5
+  flask db upgrade
+  if "$?" == "0"; then
+    break
+  fi
+  echo Deploy command failed, retrying in 5 secs...
+  sleep 5
 done
 flask translate compile
 cp supervisor.conf /etc/supervisor/conf.d/flask_quickstart.conf
